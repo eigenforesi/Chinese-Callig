0e8c1fa6fa7f86c2a8635006d16ced9659010863
Matthieu Foresi <117128751+eigenforesi@users.noreply.github.com>
2025-05-27 14:08:38 -0700

    Add files via upload

---
+++ train.py
@@ -0,0 +1,9 @@
+# Imports
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import torch
+import torchvision
+import torch.nn as nn
+from IPython.display import Image
